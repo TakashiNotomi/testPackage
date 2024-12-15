f10ccdb65595612b3926f779a6e4961dccece6db
TakashiNotomi <noutomi@gmail.com>
2024-12-16 08:17:41 +0900

    FirstCommit

---
+++ R/plus1.R
@@ -0,0 +1,15 @@
+#' @title plus1
+#' @description \code{plus1} adds 1 to the input value.
+#' @param x A numeric value or vector to which 1 will be added.
+#' @return A numeric value or vector with 1 added to each element of the input.
+#' @export
+#' @examples
+#' plus1(3) # returns 4
+#' plus1(c(1, 2, 3)) # returns c(2, 3, 4)
+
+plus1 <- function(x) {
+  return(x + 1)
+}
+
+
+
